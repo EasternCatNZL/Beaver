1c4daf0eff6d4fccbe6e8464938c5d747f9b24a5
EasternCatNZL <brianpwwong@gmail.com>
2017-03-13 15:54:22 +1300

    Fixed jump animation
    Fixed jump, started work on detecting vines.

---
+++ Beaver/Assets/Scripts/Planet/CheckVinesCollided.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckVinesCollided : MonoBehaviour {
+
+    //public float planetRadius; //radius of planet
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //CountVineHeadsInPlanet();
+        print(CountVineHeadsInPlanet());
+	}
+
+    //void OnTriggerStay(Collider _other)
+    //{
+    //    if (_other.CompareTag("VineHead"))
+    //    {
+
+    //    }
+    //}
+
+    //counts the number of vine heads in planet
+    int CountVineHeadsInPlanet()
+    {
+        int numberOfHeads = 0;
+        //get all colliders within planet area
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, GetComponent<SphereCollider>().radius);
+        for (int i = 0; i < hitColliders.Length; i++)
+        {
+            //if object is vine head, count
+            if (hitColliders[i].CompareTag("VineHead"))
+            {
+                numberOfHeads++;
+            }
+        }
+
+        return numberOfHeads;
+    }
+}
--- Beaver/Assets/Scripts/Player/FauxGravityMovement.cs
+++ Beaver/Assets/Scripts/Player/FauxGravityMovement.cs
@@ -8,32 +8,61 @@ public class FauxGravityMovement : MonoBehaviour
 
     public float moveSpeed = 15.0f; //moveSpeed at which object moves
     public float jumpForce = 200.0f; //amount of acceleration used in jumps
+    public float xBoundries = 9.0f; //horizontal boundry border
+    public float objectHeight = 2.0f; //height of the object
+    public float rayVariation = 0.2f; //wiggle room for ray calculation
+    //public Transform planet;
+    //public float distanceBetween = 0.0f;
 
     //bool m_bIsGrounded = true; //checks if object is grounded
     Vector3 m_v3MoveAmount; //amount to GetMoveInput object
     Vector3 m_v3SmoothMoveVelocity; //smooths out movement
     Rigidbody m_Rigid; //reference to this objects rigidbody
-
-    private Animator m_Animator;
+    FauxGravityBody m_fauxBody; //reference to faux body
+    bool m_bCanJump = true; //checks whether can jump
+    bool m_bCanAttack = true; //checks whether can attack
+    public bool m_bFalling = false; //checks if falling
+    public bool m_bRising = false; //check if rising
+    private Animator m_Animator; //reference to animator on object
 
     void Start()
     {
         m_Rigid = GetComponent<Rigidbody>();
         m_Animator = GetComponent<Animator>();
+        m_fauxBody = GetComponent<FauxGravityBody>();
     }
 
     void Update()
     {
         GetMoveInput();
         Jump();
-        if(GetComponent<Rigidbody>().velocity.y < 0.0f)
+        if(m_Rigid.velocity.y < 0.0f)
         {
             m_Animator.SetBool("Falling", true);
+            //if reaching ground
+            if (m_bRising)
+            {
+                m_bFalling = true;
+                m_bRising = false;
+            }
         }
         else
         {
             m_Animator.SetBool("Falling", false);
+
         }
+        if (m_bFalling)
+        {
+            Landing();
+        }
+
+        //if (CheckGrounded())
+        //{
+        //    print("On Ground");
+        //}
+        //DebugRay();
+        //distanceBetween = Mathf.Round(Vector3.Distance(transform.position, planet.position));
+        
     }
 
     void FixedUpdate()
@@ -46,17 +75,17 @@ public class FauxGravityMovement : MonoBehaviour
     {
         if (collision.gameObject.tag == "Vine" || collision.gameObject.tag == "VineHead" || collision.gameObject.tag == "VineRoot")
         {
-            m_Animator.SetBool("Attack", true);
+            m_Animator.SetTrigger("Attacking");
         }
     }
 
-    void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.tag == "Vine" || collision.gameObject.tag == "VineHead" || collision.gameObject.tag == "VineRoot")
-        {
-            m_Animator.SetBool("Attack", false);
-        }
-    }
+    //void OnCollisionExit(Collision collision)
+    //{
+    //    if (collision.gameObject.tag == "Vine" || collision.gameObject.tag == "VineHead" || collision.gameObject.tag == "VineRoot")
+    //    {
+    //        m_Animator.SetTrigger("Attacking");
+    //    }
+    //}
 
     //gets movement for controlled object
     void GetMoveInput()
@@ -67,6 +96,8 @@ public class FauxGravityMovement : MonoBehaviour
 
         //set GetMoveInput direction
         Vector3 moveDir = new Vector3(inputX, 0, 0).normalized;
+
+        //change animations based on horizontal input
         if(moveDir.x > 0)
         {
             m_Animator.SetBool("Right", true);
@@ -82,9 +113,31 @@ public class FauxGravityMovement : MonoBehaviour
             m_Animator.SetBool("Right", false);
             m_Animator.SetBool("Left", false);
         }
-        //get the amount to move
-        Vector3 targetMoveAmount = moveDir * moveSpeed;
 
+        Vector3 targetMoveAmount = new Vector3(0,0,0);
+
+        //get the amount to move, limited by boundries
+        if (AtLeftBoundry())
+        {
+            //if input is not left
+            if (moveDir.x > 0)
+            {
+                targetMoveAmount = moveDir * moveSpeed;
+            }
+        }
+        else if (AtRightBoundry())
+        {
+            //if input is not right
+            if (moveDir.x < 0)
+            {
+                targetMoveAmount = moveDir * moveSpeed;
+            }
+        }
+        //else no restrictions
+        else
+        {
+            targetMoveAmount = moveDir * moveSpeed;
+        }
         m_v3MoveAmount = Vector3.SmoothDamp(m_v3MoveAmount, targetMoveAmount, ref m_v3SmoothMoveVelocity, 0.15f);
     }
 
@@ -93,10 +146,13 @@ public class FauxGravityMovement : MonoBehaviour
     {
         if (Input.GetButtonDown("Jump"))
         {
-            if (CheckGrounded())
+            //if currently on ground, then jump
+            if (CheckGrounded() && m_bCanJump)
             {
                 m_Animator.SetTrigger("Jump");
                 m_Rigid.AddForce(transform.up * jumpForce);
+                m_bCanJump = false;
+                m_bRising = true;
             }
         }
     }
@@ -104,16 +160,82 @@ public class FauxGravityMovement : MonoBehaviour
     //checks to see if object is currently on the ground
     bool CheckGrounded()
     {
+        bool onGround = false;
         Ray ray = new Ray(transform.position, -transform.up);
         RaycastHit hit;
+        //if ray detects ground
+        if (Physics.Raycast(ray, out hit, objectHeight / 2 + rayVariation))
+        {
+            //print(hit.distance);
+            //check if distance is less then object height
+            if (hit.collider.tag == "Ground")
+            {
+                onGround = true;
+            }
+            //if (hit.distance <= objectHeight / 2 + rayVariation)
+            //{
+            //    onGround = true;
+            //}
+        }
+        return onGround;
+    }
 
-        if (Physics.Raycast(ray, out hit, 5.5f + 0.2f))
+    //checks if object has reached left boundry
+    bool AtLeftBoundry()
+    {
+        bool atEdge = false;
+        //check if reached left edge
+        if (transform.position.x < -xBoundries)
         {
-            return true;
+            atEdge = true;
         }
-        else
+        return atEdge;
+    }
+
+    //checks if object has reached right boundry
+    bool AtRightBoundry()
+    {
+        bool atEdge = false;
+        //check if reached right edge
+        if (transform.position.x > xBoundries)
         {
-            return false;
+            atEdge = true;
         }
+        return atEdge;
+    }
+
+    //logic for attacking
+    void Attack()
+    {
+        //stops object in the air
+        m_Rigid.velocity = Vector3.zero;
+        //change to attacking
+        m_Animator.SetTrigger("Attacking");
+        //change 
+    }
+
+    //animation event logic for landing
+    void Landing()
+    {
+        if (CheckGrounded() && m_bFalling && m_Animator.GetCurrentAnimatorStateInfo(0).IsName("Falling"))
+        {
+            //change to landing
+            m_Animator.SetTrigger("Landing");
+            m_bFalling = false;
+        }
+    }
+
+    //change ready to jump after landing
+    void ReadyToJump()
+    {
+        m_bCanJump = true;
+        
+    }
+
+    void DebugRay()
+    {
+        
+        Debug.DrawRay(transform.position, -transform.up * (objectHeight + rayVariation));
+
     }
 }
