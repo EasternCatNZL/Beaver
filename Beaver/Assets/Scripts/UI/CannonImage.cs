18c568ae41794eaa59546e1769060a85f46e7a67
EasternCatNZL <brianpwwong@gmail.com>
2017-03-15 02:50:18 +1300

    finished cannon buy image change

--- Beaver/Assets/Scripts/UI/CannonImage.cs
+++ Beaver/Assets/Scripts/UI/CannonImage.cs
@@ -1,8 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CannonImage : MonoBehaviour {
+
+    public Image image; //reference to image
+    public Sprite canBuyImage; //reference to lit image
+    public Sprite cantBuyImage; //reference to greyed image
+    public CurrencyManagement currencyManager; //reference to currency manager
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +17,13 @@ public class CannonImage : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (currencyManager.CanBuyTurret(CurrencyManagement.TurretTypes.BEAVERLAUNCHER))
+        {
+            image.sprite = canBuyImage;
+        }
+        else
+        {
+            image.sprite = canBuyImage;
+        }
 	}
 }
