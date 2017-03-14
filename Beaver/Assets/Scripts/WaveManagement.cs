b0ec976df2ca3cef2e26d393ece9e24f05cb69b6
EasternCatNZL <brianpwwong@gmail.com>
2017-03-14 15:48:45 +1300

    Quick background thing
    moving background, saved as a prefab

--- Beaver/Assets/Scripts/WaveManagement.cs
+++ Beaver/Assets/Scripts/WaveManagement.cs
@@ -8,7 +8,7 @@ public class WaveManagement : MonoBehaviour {
     public bool TimerBased;
     public bool ElminBased;
 
-    private int WaveNumber = 0;
+    private int WaveNumber = 1;
     private int[] Fib = { 1, 1, 2, 3, 5, 8, 13, 21, 34 };
     private float m_fTimer = 0.0f;
 
@@ -38,7 +38,6 @@ public class WaveManagement : MonoBehaviour {
 
     void NextWave()
     {
-        Vine.SetMaxSplits(10);
         if (WaveNumber < Fib.Length)
         {
             SpawnVineRoots(Fib[WaveNumber]);
@@ -48,19 +47,14 @@ public class WaveManagement : MonoBehaviour {
 
     void SpawnVineRoots(int _iAmount)
     {
-        float _fWidthIntervals = 1.0f / (_iAmount + 1);
+        float _fWidthIntervals = 1.0f / _iAmount;
         float _fXPos = _fWidthIntervals;
         float _fYPos = 1.0f;
         Vector3 SpawnPos;
-        
         for(int i = 0; i < _iAmount; ++i)
         {
             SpawnPos = new Vector3(_fXPos, _fYPos, 0.0f);
-            print(SpawnPos.ToString());
             SpawnPos = Camera.main.ViewportToWorldPoint(SpawnPos);
-            print(SpawnPos.ToString());
-            SpawnPos = Vector3.Scale(SpawnPos, new Vector3(1.0f, 1.0f, 0.0f));
-            print(SpawnPos.ToString());
             Instantiate(VinePrefab, SpawnPos, Quaternion.identity);
             _fXPos += _fWidthIntervals;
         }
